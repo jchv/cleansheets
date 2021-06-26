05b3341eb03c27a63bc21990f3e2144c30e359a1
John Chadwick <john@jchw.io>
2021-06-26 13:39:28 -0700

    Update parser demo to work on Safari... maybe.

--- web/build.sh
+++ web/build.sh
@@ -7,5 +7,5 @@ cp ./web/index.html ./dist
 # Parser demo
 mkdir -p ./dist/parser
 GOOS=js GOARCH=wasm go build -o ./dist/parser/parser.wasm ./web/parser
-cp ./web/parser/parser.html ./dist/parser/parser.html
+cp ./web/parser/index.html ./dist/parser/index.html
 cp ./web/parser/wasm_exec.js ./dist/parser/wasm_exec.js
