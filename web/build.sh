a85f903e709f4646bba17e90ff11952f02f40e9d
John Chadwick <john@jchw.io>
2021-06-26 11:52:41 -0700

    Make build script build from root.

--- web/build.sh
+++ web/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-mkdir -p ../dist
-cp ./index.html ../dist
+mkdir -p ./dist
+cp ./web/index.html ../dist
 
 # Parser demo
-mkdir -p ../dist/parser
-GOOS=js GOARCH=wasm go build -o ../dist/parser/parser.wasm ./parser
-cp ./parser/parser.html ../dist/parser/parser.html
-cp ./parser/wasm_exec.js ../dist/parser/wasm_exec.js
+mkdir -p ./dist/parser
+GOOS=js GOARCH=wasm go build -o ./dist/parser/parser.wasm ./web/parser
+cp ./web/parser/parser.html ./dist/parser/parser.html
+cp ./web/parser/wasm_exec.js ./dist/parser/wasm_exec.js
